ccf872d8ca220a6d26f9defa6d0a611cb63de19e
exec zsh <fsuhas@gmx.com>
2022-05-30 16:07:06 +0200

    Create network event booking and validates

---
+++ app/models/network.rb
@@ -0,0 +1,5 @@
+class Network < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true, uniqueness: true
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,43 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_30_115056) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_30_132700) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "nb_guest"
+    t.boolean "confirmation"
+    t.bigint "event_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_bookings_on_event_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "title"
+    t.string "type"
+    t.string "address"
+    t.text "description"
+    t.datetime "date"
+    t.string "language"
+    t.integer "capacity"
+    t.float "rating"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
+
+  create_table "networks", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_networks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +56,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_30_115056) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "avatar"
+    t.string "address"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "events"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "events", "users"
+  add_foreign_key "networks", "users"
 end
