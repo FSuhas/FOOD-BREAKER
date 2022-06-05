3b35360f6ed86bab2bda63f669839730372ff75e
exec zsh <fsuhas@gmx.com>
2022-06-05 22:59:10 +0200

    dodo

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit]
+  skip_before_action :authenticate_user!, only: [ :show ]
 
   def index
     if params[:address].present?
